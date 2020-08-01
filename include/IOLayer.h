6946179b0b109361942c356959627dbe0fa9a8de
RicardoLuis0 <ricolvs123@gmail.com>
2020-08-01 02:38:25 -0300

    colors

--- include/IOLayer.h
+++ include/IOLayer.h
@@ -4,6 +4,24 @@
 
 namespace IO {
     void init();
+    enum color {
+        BLACK = 0,
+        BLUE = 1,
+        GREEN = 2,
+        CYAN = 3,
+        RED = 4,
+        MAGENTA = 5,
+        BROWN = 6,
+        LIGHT_GREY = 7,
+        DARK_GREY = 8,
+        LIGHT_BLUE = 9,
+        LIGHT_GREEN = 10,
+        LIGHT_CYAN = 11,
+        LIGHT_RED = 12,
+        LIGHT_MAGENTA = 13,
+        LIGHT_BROWN = 14,
+        WHITE = 15,
+    };
     enum key_type {
         ARROW_LEFT,
         ARROW_UP,
@@ -19,4 +37,5 @@ namespace IO {
     keypress get_key();
     void moveCursor(int16_t x,int16_t y);
     void writeChar(char c);
+    void setColor(color fg,color bg);
 };
